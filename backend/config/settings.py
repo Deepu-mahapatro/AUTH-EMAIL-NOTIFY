d4abc05d7befbd1576ccb18c6542698a21dbf6bf
deepu <deepumahapatro2006@gmail.com>
2026-08-24 23:51:03 +0530

    Configure production host settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,8 +29,15 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "django-google-authentication.onrender.com",
+]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://django-google-authentication.onrender.com",
+]
 
 # Application definition
 
